e9978da81b406e898aa4ef1739af38d17116beb7
hajdus <32811798+hajdus@users.noreply.github.com>
2020-09-04 08:38:54 +0200

    Update DownDetector Tmobile.sh

--- DownDetector - Tmobile/DownDetector Tmobile.sh
+++ DownDetector - Tmobile/DownDetector Tmobile.sh
@@ -1,24 +1,6 @@
-curl "https://downdetector.pl/status/t-mobile/" -L --compressed -s | grep 'Nie ma problemÃ³w z T-Mobile' && [[ $? == 0 ]] && echo 0 || echo 1
-
-curl "https://downdetector.pl/status/t-mobile/" -L --compressed -s | grep -q "title='Strona glówna'>
-      Potencjalna awaria z T-Mobile" && [[ $? == 0 ]] && echo 1 || echo 0
-
-
-curl "https://downdetector.pl/status/t-mobile/" -L --compressed -s | grep -q "title='Strona glówna'>
-      Awaria w T-Mobile" && [[ $? == 0 ]] && echo 1 || echo 0
-
-
-
-
-UserParameter=curl[*],curl -L --compressed -s $1  | grep -q "title='Strona glówna'>
-      Potencjalna awaria z T-Mobile" && [[ $? == 0 ]] && echo 1 || echo 0
-
-
-UserParameter=curl_awaria[*],curl -L --compressed -s $1  | grep -q "title='Strona glówna'>
-      Awaria w T-Mobile" && [[ $? == 0 ]] && echo 1 || echo 0
-
+UserParameter=curl[*],/bin/bash -c "curl -L --compressed -s $1 |tr '\n' ' '  |tr  $'\'' ' ' |  grep -q   'title= Strona gÅ‚Ã³wna >       Potencjalna awaria z T-Mobile' && [[ $? == 0 ]] && echo 1 || echo 0"
+UserParameter=curl_awaria[*],/bin/bash -c "curl -L --compressed -s $1 |tr '\n' ' '  |tr  $'\'' ' ' |  grep -q   'title= Strona gÅ‚Ã³wna >        Awaria w T-Mobile' && [[ $? == 0 ]] && echo 1 || echo 0"
 
 
 zabbix_get -s 10.110.12.115 -k "curl[https://downdetector.pl/status/t-mobile/]"
-
 
