a0384e2157b29aab3011d4aeef6c3c6bc97ec702
hajdus <32811798+hajdus@users.noreply.github.com>
2020-09-28 10:59:40 +0200

    Update DownDetector Tmobile.sh

--- DownDetector - Tmobile/DownDetector Tmobile.sh
+++ DownDetector - Tmobile/DownDetector Tmobile.sh
@@ -1,5 +1,6 @@
 UserParameter=curl[*],/bin/bash -c "curl -L --compressed -s $1 |tr '\n' ' '  |tr  $'\'' ' ' |  grep -q   'title= Strona główna >       Potencjalna awaria z T-Mobile' && [[ $? == 0 ]] && echo 1 || echo 0"
-UserParameter=curl_awaria[*],/bin/bash -c "curl -L --compressed -s $1 |tr '\n' ' '  |tr  $'\'' ' ' |  grep -q   'title= Strona główna >        Awaria w T-Mobile' && [[ $? == 0 ]] && echo 1 || echo 0"
+
+UserParameter=curl_awaria[*],/bin/bash -c "curl -L --compressed -s $1 |tr '\n' ' '  |tr  $'\'' ' ' |  grep -q   'title= Strona główna >                </a>            <div class= color-danger h2 entry-title >            Awaria w T-Mobile' && [[ $? == 0 ]] && echo 1 || echo 0"
 
 
 zabbix_get -s 10.110.12.115 -k "curl[https://downdetector.pl/status/t-mobile/]"
